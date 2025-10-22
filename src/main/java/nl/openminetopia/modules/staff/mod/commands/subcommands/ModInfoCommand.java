a96d50da378c578f692da1b721bf9f8ff843908b
Rune <rune.haest@gmail.com>
2025-10-22 18:37:29 +0200

    Task #62

--- src/main/java/nl/openminetopia/modules/staff/mod/commands/subcommands/ModInfoCommand.java
+++ src/main/java/nl/openminetopia/modules/staff/mod/commands/subcommands/ModInfoCommand.java
@@ -2,15 +2,23 @@ package nl.openminetopia.modules.staff.mod.commands.subcommands;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
 import nl.openminetopia.DailyLife;
 import nl.openminetopia.api.player.PlayerManager;
 import nl.openminetopia.modules.player.events.PlayerLevelChangeEvent;
+import nl.openminetopia.modules.plots.PlotModule;
 import nl.openminetopia.modules.prefix.models.PrefixModel;
 import nl.openminetopia.modules.prefix.objects.Prefix;
 import nl.openminetopia.utils.ChatUtils;
 import nl.openminetopia.utils.events.EventUtils;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+
+import java.util.*;
 
 @CommandAlias("mod")
 public class ModInfoCommand extends BaseCommand {
@@ -76,10 +84,33 @@ public class ModInfoCommand extends BaseCommand {
                     DailyLife.getMessageConfiguration().message("multi_message_category", player)
                             .replace("<text>", DailyLife.getMessageConfiguration().message("staff_mod_info_plot_category", player))
             ));
-            player.sendMessage(ChatUtils.format(minetopiaPlayer,
-                    DailyLife.getMessageConfiguration().message("multi_message_list", player)
-                            .replace("<text>", "<red>Plots are not being processed jet.")
-            ));
+
+            World world = BukkitAdapter.adapt(player.getWorld());
+
+            RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+            RegionManager manager = container.get(world);
+
+            if (manager == null) {
+                player.sendMessage(ChatUtils.format(minetopiaPlayer, "<red>Er ging iets mis met het ophalen van de regio's."));
+                return;
+            }
+
+            Map<String, String> regionNames = new HashMap<>();
+            manager.getRegions().entrySet().stream()
+                    .filter(entry -> entry.getValue().getFlag(PlotModule.PLOT_FLAG) != null)
+                    .filter(entry -> entry.getValue().getOwners().contains(offlinePlayer.getUniqueId()) || entry.getValue().getMembers().contains(offlinePlayer.getUniqueId()))
+                    .forEach(entry -> regionNames.put(entry.getKey(), entry.getValue().getOwners().contains(offlinePlayer.getUniqueId()) ? "Owner" : "Member"));
+            List<Map.Entry<String, String>> entries = new ArrayList<>(regionNames.entrySet());
+
+
+
+            for(int i = 1; i < entries.size(); i++){
+                Map.Entry<String, String> entry = entries.get(i);
+                player.sendMessage(ChatUtils.format(minetopiaPlayer,
+                        DailyLife.getMessageConfiguration().message("multi_message_list", player)
+                                .replace("<text>", entry.getKey() + " <gray>(" + entry.getValue() + ")")
+                ));
+            }
 
         });
     }
